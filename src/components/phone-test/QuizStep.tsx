7e736cf532b3a49c249c51b3a58505cc3ef3d4c8
asemraza <raza.asem@gmail.com>
2026-05-10 17:07:14 +0200

    style: remove 'Vi vurderer jeres svar' and icon from insight screen

--- src/components/phone-test/QuizStep.tsx
+++ src/components/phone-test/QuizStep.tsx
@@ -35,16 +35,7 @@ export const QuizStep = ({
     <div className="flex flex-col min-h-[520px] md:min-h-[580px]">
       {isNavigating && currentInsight ? (
         <div className="flex flex-1 flex-col items-center justify-center space-y-10 animate-in fade-in zoom-in-95 duration-500 py-10 px-4 text-center">
-          <div className="space-y-4">
-            <div className="flex justify-center">
-              <div className="h-10 w-10 rounded-full bg-[#151515]/5 flex items-center justify-center">
-                <Lightbulb className="h-5 w-5 text-[#151515]/40" />
-              </div>
-            </div>
-            <p className="text-[11px] font-bold uppercase tracking-[0.2em] text-[#151515]/50">
-              Vi vurderer jeres svar
-            </p>
-          </div>
+
 
           <div className="space-y-4 mx-auto max-w-[32ch]">
             {headline && (
